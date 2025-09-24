2090ca85fbb5ff195daad4e629e2de47740733b7
gingi123 <gingi19910313@gmail.com>
2025-09-24 10:36:06 +0200

    teszt 1 commit

---
+++ vite-project/teszt.js
@@ -0,0 +1,6 @@
+// na az lett volna a lenyeg hogy bejelntkezuztessen ez a szar github 
+// most kesz a "git repo" es ezentul ha valtoztatsz es meg akarod mutatni / el akarod "menteni" ezt ko csinlani
+// Mit? tul gyors volt hol a commit? 
+mutatom
+ha uj file akkor itt prios lesz , ilyenkor hozza kell adni igy nem nem az egy pillanat megnzek valamit
+erdekes elnevezes m1
